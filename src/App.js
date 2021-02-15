0da923c38cf30e8a7a7f076c5bf33c32dcc300c2
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2021-02-15 17:50:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Brazil from '@svg-maps/brazil';
+import { SVGMap, CheckboxSVGMap, RadioSVGMap } from 'react-svg-map';
+import "react-svg-map/lib/index.css";
+import './style.css';
 
 function App() {
+
+  const [ BrazilPreSet, setBrasil ]= useState({
+    ...Brazil,
+    label: 'Custom map label'
+  });
+
+  useEffect(() => {
+    setBrasil({
+      ...BrazilPreSet,
+      locations: Brazil.locations.map(location => {
+      
+        setTimeout(() => {
+          let newarr = {
+            ce: {
+              fill: '#1e30ba'
+            },
+            ro: {
+              fill: '#1e30ba'
+            },
+            rr: {
+              fill: '#1e30ba'
+            },
+            sp: {
+              fill: '#1e30ba'
+            },
+            rs: {
+              fill: '#1e30ba'
+            }
+          };
+          for(let k in newarr){
+            if(location.id == k){
+              document.getElementById(location.id).style.fill = newarr[k].fill;
+            }
+          }
+        }, 100);
+  
+        return location;
+      })
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SVGMap className="aps"  map={BrazilPreSet} />
+    // onLocationFocus={(e) => {alert(e.nativeEvent.srcElement.ariaLabel)}}
   );
 }
 
